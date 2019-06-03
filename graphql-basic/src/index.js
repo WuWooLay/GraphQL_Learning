4a2820c5c3cb61cd88b467fb11a102a7fe4caf70
WuWooLay <mamamglay.lp@gmail.com>
2019-06-03 14:00:02 +0630

    Add GraphQl Learning Basic

--- graphql-basic/src/index.js
+++ graphql-basic/src/index.js
@@ -1,5 +1,27 @@
-import defaultAdd, { substract } from './math'
+import { GraphQLServer } from 'graphql-yoga'
 
-console.log(substract(3, 2))
+// Type Definitions
+const typeDefs = `
+    type Query {
+        hello: String!,
+        location: String!,
+        bio: String!
+    }
+`
 
-console.log(defaultAdd(1, 2))
+//  Resolvers
+const resolvers = {
+	Query: {
+		hello: () => 'SOrry this is Hello',
+		location: () => ' No 522(A) South Dagon 19 ward Ygn',
+		bio: () => ' My Bio is Not Biology '
+	}
+}
+
+// GraphQl Server Start
+const server = new GraphQLServer({
+	typeDefs,
+	resolvers
+})
+
+server.start(() => console.log(`Server Start Port 4000`))
